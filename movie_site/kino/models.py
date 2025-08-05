56d1f3f3c74e53d89557aef5cfa8033348359d46
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-05 17:09:25 +0600

    Update models.py
    k;l

--- movie_site/kino/models.py
+++ movie_site/kino/models.py
@@ -1,7 +1,7 @@
 from django.core.validators import MinValueValidator 
 from django.db import models 
 from django.db.models import PositiveSmallIntegerField 
-from django.contrib.auth.models import  AbstractUser
+from django.contrib.auth.models import  AbstractUser 
 from phonenumber_field.modelfields import PhoneNumberField
 from multiselectfield import MultiSelectField
 
