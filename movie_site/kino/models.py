5a7baed17186c58ced2be74956f5cb82b1b32a70
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-05 17:08:50 +0600

    Update models.py
    gdf

--- movie_site/kino/models.py
+++ movie_site/kino/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator 
 from django.db import models
 from django.db.models import PositiveSmallIntegerField
 from django.contrib.auth.models import  AbstractUser
